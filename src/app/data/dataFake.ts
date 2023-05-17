78f5c052abb9ebaf6677fde7e1906357883632db
IvambergSilva <ivambergisilva@gmail.com>
2023-05-17 00:01:36 -0300

    Conteúdo dinâmico atualizado

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,5 +10,17 @@ export const dataFake = [
         'photoCover':'https://th.bing.com/th/id/OIP.AdD5USyN3gk_mUGuGGgqlAHaEo?pid=ImgDet&rs=1',
         'title': 'Novo filme do Pantera Negra',
         'description': 'Não perca esse grande sucesso!'
-    }
+    },
+    {
+        'id': "3",
+        'photoCover':'https://th.bing.com/th/id/OIP.PizL3Mmxtu1ypbFiKcuDPQHaEK?pid=ImgDet&rs=1',
+        'title': 'Veja o filme do Homem de Ferro 4',
+        'description': 'Não perca esse grande sucesso!'
+    },
+    {
+        'id': "4",
+        'photoCover':'https://img.elo7.com.br/product/zoom/20CC0ED/painel-homem-aranha-g-frete-gratis-tematicas.jpg',
+        'title': 'Vem aí: Homem-Aranha',
+        'description': 'Não perca esse grande sucesso!'
+    },
 ]
